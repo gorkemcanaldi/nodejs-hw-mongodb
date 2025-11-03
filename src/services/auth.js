91e416b29f77913f5481cfc245ac8190392bc87a
gorkem can aldi <gorkem.aldi2003@gmail.com>
2025-11-04 00:51:42 +0300

    Fix reset password email link

--- src/services/auth.js
+++ src/services/auth.js
@@ -116,7 +116,7 @@ export const requestResetMail = async (email) => {
 
   const htmlMailIcerik = template({
     name: user.name,
-    url: `${process.env.APP_DOMAIN}/reset-pwd?token=${resetToken}`,
+    url: `${process.env.APP_DOMAIN}/reset-password?token=${resetToken}`,
   });
 
   // mail gönder
